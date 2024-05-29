faa0dc37d43821b16045a29bbc8cbcf6677c2966
whisperblower <0x5f0x300x660x66@gmail.com>
2024-05-29 14:26:08 +0200

    Update sum-numbers.c

--- sum-numbers.c
+++ sum-numbers.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main()
+{
+    int i_operand1 = 0; 
+    int i_operand2 = 0;
+
+    float f_operand1 = 0;
+    float f_operand2 = 0;
+
+    scanf("%d %d\n", &i_operand1, &i_operand2);
+    scanf("%f %f\n", &f_operand1, &f_operand2);
+    
+    printf("%d %d\n", i_operand1 + i_operand2, i_operand1 - i_operand2);
+    printf("%.1f %.1f\n", f_operand1 + f_operand2, f_operand1 - f_operand2);
+
+    return 0;
+}
