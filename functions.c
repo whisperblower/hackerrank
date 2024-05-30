19586da29caa83f0a9ce57fde17cb0f6f4034d12
whisperblower <0x5f0x300x660x66@gmail.com>
2024-05-30 12:44:43 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int max(int x, int y) 
+{
+    return (x > y) ? x : y;
+}
+
+int max_of_four(int a, int b, int c, int d) 
+{
+    int max_ab = max(a, b);
+    int max_cd = max(c, d);
+
+    return max(max_ab, max_cd);
+}
+
+int main() 
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
